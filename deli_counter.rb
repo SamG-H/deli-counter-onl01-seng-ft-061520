f52cfcafc89758d3ed92f4a7e126de58b8520f6d
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-06-17 15:15:23 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+# frozen_string_literal: false
+
+def line(line)
+  if line.empty?
+    puts 'The line is currently empty.'
+  else
+    message = 'The line is currently: '
+    line.each do |customer|
+      message << "#{line.index(customer) + 1}. #{customer} "
+    end
+    puts message.delete_suffix(' ')
+  end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.index(name) + 1} in line."
+end
+
+def now_serving(line)
+  puts 'There is nobody waiting to be served!' if line.empty?
+  puts "Currently serving #{line.first}." unless line.empty?
+  line.shift
+end
